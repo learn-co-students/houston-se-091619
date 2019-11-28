c8ef490c6d010b31ecd94a15c78d2c8d369af663
vidhisharma3193 <vidhisharma3193@gmail.com>
2019-11-27 22:34:59 -0600

    done

--- 38-State-Forms-Events/src/PaintingForm.js
+++ 38-State-Forms-Events/src/PaintingForm.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 class PaintingForm extends React.Component{
+
+    handleSubmit = () => {
+        this.props.history.push("/paintings")
+    }
+
     render(){
         // console.log(this.props)
         return(<div>
-            <form onSubmit={(e) => this.props.addPainting(e)}>
+            <form onSubmit={(e) => {
+                this.props.addPainting(e)
+                this.handleSubmit()
+                }}>
                 <input type='text' placeholder='Painting Title'/>
                 <input type='text' placeholder='Painting Image URL'/>
                 <input type='text' placeholder='Artist Name'/>
                 <input type='submit' value='Add Painting'/>
             </form>
+            <Link to="/paintings">Go to Home</Link>
         </div>)
     }
 }
